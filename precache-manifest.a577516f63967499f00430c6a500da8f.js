25e2e9011a36d90c1ea678e57b91074c7614f597
Mariño Carranza <marinocarranza@Marinos-MacBook-Pro.local>
2019-11-11 16:00:45 -0800

    Updates

---
+++ precache-manifest.a577516f63967499f00430c6a500da8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a3cb73957e6a30c973f34b1381d388",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "44ea03f09e3d1fda00ed",
+    "url": "/memory-game/static/css/main.5f6a0605.chunk.css"
+  },
+  {
+    "revision": "206ef4bb2048135436a1",
+    "url": "/memory-game/static/js/2.3c412a7e.chunk.js"
+  },
+  {
+    "revision": "44ea03f09e3d1fda00ed",
+    "url": "/memory-game/static/js/main.a9629e27.chunk.js"
+  },
+  {
+    "revision": "b9072d66a05035e4de66",
+    "url": "/memory-game/static/js/runtime-main.0329a550.js"
+  }
+]);
